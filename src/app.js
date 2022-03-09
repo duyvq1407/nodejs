51e3bad2dbe07a23a7a88da660c25c3e1b7b0b72
Vũ Quang Duy <vduy2350@gmail.com>
2022-03-10 03:32:15 +0700

    update

--- src/app.js
+++ src/app.js
@@ -20,11 +20,28 @@ app.get('/api/products', checkAuth, (req, res) => {
     const data = [
         {
             id: 1,
-            name: "Product 1"
+            name: "Product 1",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfX71_TAsDlQuGGDciBYwX5IKAQJcZauxy4qSuhcxO5TlYECRRrqP8dtsoCFvR1NIXbn0&usqp=CAU"
         },
         {
             id: 2,
-            name: "Product 2"
+            name: "Product 2",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfFhj2AmdBUAjp5VW45uJSeMehluBZvdfFKg&usqp=CAU"
+        },
+        {
+            id: 3,
+            name: "Product 3",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiTKNpyKFX7zzsCTGNEikWbmmbkj_Hibeb5ZPDX-LBmx7jv5mUR-ADiTuRM4L4LMm-sYg&usqp=CAU"
+        },
+        {
+            id: 4,
+            name: "Product 4",
+            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHHvu30Czk0i_q7aJqB_mJSfnV7aiMdEZ_nDhZA4ugFdjcOiM_kXkdipdvMEqMd0QfFA8&usqp=CAU"
+        },
+        {
+            id: 5,
+            name: "Product 5",
+            img: "https://hinhnen123.com/wp-content/uploads/2021/06/Bo-suu-tap-99-hinh-nen-dien-thoai-cute-ngo-nginh-nhat.jpg"
         },
     ];
     res.json(data);
@@ -37,7 +54,7 @@ app.get('/api/categories', checkAuth, (req, res) => {
         },
         {
             id: 2,
-            name: "Category 3"
+            name: "Category 2"
         },
     ];
     res.json(data);
