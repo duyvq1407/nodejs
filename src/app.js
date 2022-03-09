b106374c040e5e3daf949c9636b7f0aaca0ffb82
Vũ Quang Duy <vduy2350@gmail.com>
2022-03-10 03:06:13 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,44 +1,65 @@
 // const http = require('http');
 const express = require('express');
-const app = express();
-app.get('/')
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    console.log(url);
-    if (url === "/api/products") {
-        const data = [
-            {
-                id: 1,
-                name: "Product"
-            },
-            {
-                id: 2,
-                name: "Product  2"
-            },
-        ]
-        res.end(JSON.stringify(data))
-    } 
-    else if (url === "/api/categories") {
-        const data = [
-            {
-                id: 1,
-                name: "Category 1"
-            },
-            {
-                id: 2,
-                name: "Category 2"
-            },
-        ]
-        res.end(JSON.stringify(data))
-    }
-    else {
-        res.setHeader("Content-Type", "text/html");
-        res.write("<html><body><h1>Xin Chao</h1></body></html>");
-        res.end()
-    }
-})
+const cors = require('cors');
 
-const PORT = 3001;
-server.listen(PORT, () => {
-    console.log("Bạn đang chạy cổng " + PORT)
+const app = express();
+app.use(cors())
+
+const checkAuth = (req, res,next) => {
+    const isAdmin = true;
+    if(isAdmin){
+        next();
+    } else {
+        console.log('Chim cút');
+    }
+}
+app.get('/', (req, res) => {
+    res.send("<h1>Home Page</h1>")
 })
+app.get('/api/products', checkAuth, (req, res) => {
+    const data = [
+        {
+            id: 1,
+            name: "Product 1"
+        },
+        {
+            id: 2,
+            name: "Product 2"
+        },
+    ];
+    res.json(data);
+});
+app.get('/api/categories', checkAuth, (req, res) => {
+    const data = [
+        {
+            id: 1,
+            name: "Category 1"
+        },
+        {
+            id: 2,
+            name: "Category 3"
+        },
+    ];
+    res.json(data);
+});
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     console.log(url);
+//     if(url === "/api/products"){
+//         const data = [
+//             {id: 1, name: "Product A"}, 
+//             {id: 2, name: "Product B"}
+//         ];
+//         res.end(JSON.stringify(data))
+//     } else if( url === "/api/posts"){
+//         console.log("API Post")
+//     } else {
+//         res.setHeader("Content-Type","text/html");
+//         res.write("<html><body><h1>Home Page</h1></body></html>");
+//         res.end();
+//     }
+// })
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log("Server của bạn đang chạy cổng ", PORT);
+});
